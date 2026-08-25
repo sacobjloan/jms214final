2f0fb24841a96859c98af416990c985eb91f1100
sacobjloan <sacobjloan@gmail.com>
2026-08-25 11:30:28 -0700

    uh oh

--- spaghetti.r
+++ spaghetti.r
@@ -15,7 +15,7 @@ prm <- read_csv("data/RioMameyesPuenteRoto.csv")
 
 bigdata <- bind_rows(bisley1,bisley2,bisley3, prm)
 
-bigdata <- bigdata |> 
+bigdatabigdata <- bigdata |> 
   select(Sample_Date, `NO3-N`, Mg, K, `NH4-N`, Sample_ID) |> 
   filter(year(Sample_Date)>= 1988 & year(Sample_Date) <= 1994) |> 
   arrange(Sample_ID, Sample_Date)
