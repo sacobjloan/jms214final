2e411d997c4874c4af7ac8553692ab1b5ff8d3e0
sacobjloan <sacobjloan@gmail.com>
2026-08-25 11:09:54 -0700

    add final

---
+++ finalscript.r
@@ -0,0 +1,28 @@
+# How will you get the data into R?
+  # above easy mode
+# How will you clean up the data?
+  # probably set an NA value, then take those values out
+# Are any processing or modeling steps required?
+  # Lots of summarizing, creating a moving average function
+# What are your outputs?
+  # Graph of the moving 9 week average
+
+
+# read in data and set packages ------------------------------------------
+
+library(tidyverse)
+
+bisley1 <- read_csv("data/QuebradaCuenca1-Bisley.csv")
+bisley2 <- read_csv("data/QuebradaCuenca2-Bisley.csv")
+bisley3 <- read_csv("data/QuebradaCuenca3-Bisley.csv")
+prm <- read_csv("data/RioMameyesPuenteRoto.csv")
+
+
+# clean up data ----------------------------------------------------------
+
+# remove irrelevant columns
+# 
+
+
+
+
